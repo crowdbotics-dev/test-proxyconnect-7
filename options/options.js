ac44f17702c802c80fc83181c26b7fff9b1446dd
Crowdbotics <engineering@crowdbotics.com>
2023-10-18 09:56:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-proxyconnect-7",
+  url: "https://test-proxyconnect-7.botics.co",
+  api: "https://test-proxyconnect-7.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
